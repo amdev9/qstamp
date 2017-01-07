f5f30c64aa2ecbeb51bcb9867e35f0578fe78a39
Alexander Matveev <blackkorol@gmail.com>
2017-01-07 22:58:11 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char** argv){
   if (argc == 1){
     std::cout<<"";
-    // std::cout<<"qs_timestamp generator\nUsage: qs iterations size edgePercentage shift header\n";
+    std::cout<<" [*] Quicksand generator: example usage \n [-] ./quicksand [iterations] [size] [edges] [shift] [header] \n [-] ./quicksand 10 42 100 18 VcgkAma6zq7wP4TsRGfESjoLQtriNOIB\n";
     return 1;
   }
   if (argc !=6) {
@@ -20,17 +20,7 @@ int main(int argc, char** argv){
   std::string header{argv[5]};
   auto result = quicksand::generateStamp(iterations, size, edgePercentage, shift, header);
 
-   // 7 42 100 10 eaL1905NgvXFM2j2cvTcRueCuY5giYne
 
   std::cout<<result<<std::endl;
   return 0;
-}
-
-
-// {\"shift\":10,
-// \"header\":\"eaL1905NgvXFM2j2cvTcRueCuY5giYne\",
-// \"edges\":100,
-// \"blob\":\"AQBb4754up0oFnI8ApfhA7coB1MU_jyQZwjdbw4esQpplbEepZv5TGBTN4veiKFKZyOdpyUxYbIbMkG2DfM_aKXQp0J-5kqhgrIgHhbA93T4kAd1LRQ1l8TL199xrnyljDj6Q8a4eC-nBzN6oUpkChfzaIylmlMcI500emTpq2-x4tydRjoWgZzvvfpAXuZaGBAEL88bGFII-zZRAtvTFjcUKLagd_hYzCw0OQczdl_sU_JtBwMtpx2tMjgLNroClqA\",
-// \"iterations\":7,
-// \"size\":42}",
-// }
+} 
